41621ed7c2c297dbdf89fbebbd1d5610a93a9c4a
lisvesurai <lisvesurai@gmail.com>
2020-10-04 21:24:31 -0700

    Reformat code

--- js/app.js
+++ js/app.js
@@ -1,82 +1,73 @@
 (function() {
-  var app = angular.module('listApp', ['list-directives']);
-  var defaultColors = [
-        '#ed5151',
-        '#ed8551',
-        '#edb951',
-        '#eded51',
-        '#b9ed51',
-        '#85ed51',
-        '#51ed51',
-        '#51ed85',
-        '#51edb9',
-        '#51eded',
-        '#51b9ed',
-        '#5185ed',
-		'#5151ed',
-		'#8551ed',
-		'#b951ed',
-		'#ed51ed',
-		'#ed51b9',
-		'#ed5185',
-		'#909090',
-		'#eeeeee'
-  ];
-
-  app.controller('ListController', function($scope) {
-		$scope.saved = localStorage.getItem('lists');
-		$scope.lists = (localStorage.getItem('lists')!==null) ? JSON.parse($scope.saved) : [];
-		$scope.colors = defaultColors;
-		localStorage.setItem('lists', JSON.stringify($scope.lists));
-
-		$scope.add = function(tab) {
-			$scope.lists.push({
-				name: $scope.listName,
-				listitems: [],
-				color: $scope.colors[Math.floor(Math.random() * $scope.colors.length)]
-			});
-			$scope.listName = '';
-			localStorage.setItem('lists', JSON.stringify($scope.lists));
-			tab.setTab($scope.lists.length-1);
-			window.location='#newlist';
-		};
-			
- 		this.del = function($index) {
-			$scope.lists.splice($index,1);
-			localStorage.setItem('lists', JSON.stringify($scope.lists));
-    };
-    
-		this.changeColor = function(picker) {
-			picker.setPicker(-1);
-			localStorage.setItem('lists', JSON.stringify($scope.lists));
-		};
-  });
-
-  app.controller('ListitemController', function($scope) {
-    this.listitem = {};
-
-    this.add = function(list) {
-			if (this.listitem.text!==undefined){
-      	list.listitems.push({
-					text: this.listitem.text,
-					done: false
-				});
-			}
-      this.listitem = {};
-			localStorage.setItem('lists', JSON.stringify($scope.lists));
-    };
-		
-    this.del = function(list,$index) {
-			list.listitems.splice($index,1);
-			localStorage.setItem('lists', JSON.stringify($scope.lists));
-    };
-
-    this.mark = function() {
-			localStorage.setItem('lists', JSON.stringify($scope.lists));
-    };
-  });
-	
-  app.controller('ColorPickerController', function($scope) {
-		$scope.colors = defaultColors;
-  });
+    var app = angular.module('listApp', ['list-directives']);
+    var defaultColors = [
+    	'#ed5151', 
+    	'#ed8551', 
+    	'#edb951', 
+    	'#eded51', 
+    	'#b9ed51', 
+    	'#85ed51', 
+    	'#51ed51', 
+    	'#51ed85', 
+    	'#51edb9', 
+    	'#51eded', 
+    	'#51b9ed', 
+    	'#5185ed', 
+    	'#5151ed', 
+    	'#8551ed', 
+    	'#b951ed', 
+    	'#ed51ed', 
+    	'#ed51b9', 
+    	'#ed5185', 
+    	'#909090', 
+    	'#eeeeee'
+    ];
+    app.controller('ListController', function($scope) {
+        $scope.saved = localStorage.getItem('lists');
+        $scope.lists = (localStorage.getItem('lists') !== null) ? JSON.parse($scope.saved) : [];
+        $scope.colors = defaultColors;
+        localStorage.setItem('lists', JSON.stringify($scope.lists));
+        $scope.add = function(tab) {
+            $scope.lists.push({
+                name: $scope.listName,
+                listitems: [],
+                color: $scope.colors[Math.floor(Math.random() * $scope.colors.length)]
+            });
+            $scope.listName = '';
+            localStorage.setItem('lists', JSON.stringify($scope.lists));
+            tab.setTab($scope.lists.length - 1);
+            window.location = '#newlist';
+        };
+        this.del = function($index) {
+            $scope.lists.splice($index, 1);
+            localStorage.setItem('lists', JSON.stringify($scope.lists));
+        };
+        this.changeColor = function(picker) {
+            picker.setPicker(-1);
+            localStorage.setItem('lists', JSON.stringify($scope.lists));
+        };
+    });
+    app.controller('ListitemController', function($scope) {
+        this.listitem = {};
+        this.add = function(list) {
+            if (this.listitem.text !== undefined) {
+                list.listitems.push({
+                    text: this.listitem.text,
+                    done: false
+                });
+            }
+            this.listitem = {};
+            localStorage.setItem('lists', JSON.stringify($scope.lists));
+        };
+        this.del = function(list, $index) {
+            list.listitems.splice($index, 1);
+            localStorage.setItem('lists', JSON.stringify($scope.lists));
+        };
+        this.mark = function() {
+            localStorage.setItem('lists', JSON.stringify($scope.lists));
+        };
+    });
+    app.controller('ColorPickerController', function($scope) {
+        $scope.colors = defaultColors;
+    });
 })();
--- js/list-directives.js
+++ js/list-directives.js
@@ -1,84 +1,71 @@
-(function(){
-	var app = angular.module('list-directives', []);
+(function() {
+    var app = angular.module('list-directives', []);
+    app.directive("listTabs", function() {
+        return {
+            restrict: "E",
+            templateUrl: "list-tabs.html",
+            controller: function() {
+                this.tab = -1;
+                this.isSet = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+                this.setTab = function(activeTab) {
+                    this.tab = activeTab;
+                };
+            },
+            controllerAs: "tab"
+        };
+    });
+    app.directive("listItems", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "list-items.html"
+        };
+    });
+    app.directive("colorPicker", function() {
+        return {
+            restrict: "E",
+            templateUrl: "color-picker.html",
+            controller: function() {
+                this.picker = -1;
+                this.isSet = function(checkPicker) {
+                    return this.picker === checkPicker;
+                };
+                this.setPicker = function(activePicker) {
+                    this.picker = activePicker;
+                };
+            },
+            controllerAs: "picker"
+        };
+    });
+    app.directive("contenteditable", function() {
+        return {
+            restrict: "A",
+            require: "ngModel",
+            link: function(scope, element, attr, ngModel) {
+                var tabindex = parseInt(attr.tabindex);
 
-	app.directive("listTabs", function() {
-		return {
-			restrict: "E",
-			templateUrl: "list-tabs.html",
-			controller: function() {
-				this.tab = -1;
-
-				this.isSet = function(checkTab) {
-					return this.tab === checkTab;
-				};
-
-				this.setTab = function(activeTab) {
-					this.tab = activeTab;
-				};
-			},
-			controllerAs: "tab"
-		};
-	});
-
-	app.directive("listItems", function() {
-		return {
-			restrict: 'E',
-			templateUrl: "list-items.html"
-		};
-	});	
-	
-	app.directive("colorPicker", function() {
-		return {
-			restrict: "E",
-			templateUrl: "color-picker.html",
-			controller: function() {
-				this.picker = -1;
-
-				this.isSet = function(checkPicker) {
-					return this.picker === checkPicker;
-				};
-
-				this.setPicker = function(activePicker) {
-					this.picker = activePicker;
-				};
-			},
-			controllerAs: "picker"
-		};
-	});
-
-	app.directive("contenteditable", function() {
-		return {
-			restrict: "A",
-			require: "ngModel",
-			link: function(scope, element, attr, ngModel) {
-				var tabindex = parseInt(attr.tabindex);
-	
-				function read() {
-					var html = element.html();
-					html = html.replace(/&nbsp;/g, "\u00a0");
-					ngModel.$setViewValue(html);
-				}
-	
-				ngModel.$render = function() {
-					element.html(ngModel.$viewValue || "");
-				};
-	
-				element.bind("blur keyup change", function() {
-					scope.$apply(read);
-					localStorage.setItem('lists', JSON.stringify(scope.lists));
-				});
-				
-				element.on('keydown', function (event){
-					if(event.which === 13){
-						window.event.cancelBubble = true;
-						event.returnValue = false;
-						event.preventDefault();
-        		localStorage.setItem('lists', JSON.stringify(scope.lists));
-					}
-      	});
-			}
-		};
-	});
-
+                function read() {
+                    var html = element.html();
+                    html = html.replace(/&nbsp;/g, "\u00a0");
+                    ngModel.$setViewValue(html);
+                }
+                ngModel.$render = function() {
+                    element.html(ngModel.$viewValue || "");
+                };
+                element.bind("blur keyup change", function() {
+                    scope.$apply(read);
+                    localStorage.setItem('lists', JSON.stringify(scope.lists));
+                });
+                element.on('keydown', function(event) {
+                    if (event.which === 13) {
+                        window.event.cancelBubble = true;
+                        event.returnValue = false;
+                        event.preventDefault();
+                        localStorage.setItem('lists', JSON.stringify(scope.lists));
+                    }
+                });
+            }
+        };
+    });
 })();
-
